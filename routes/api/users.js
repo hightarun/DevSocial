744163be0cf633659cefdc81526137113559f267
DaemOn25 <tarunbisht252000@gmail.com>
2020-06-29 12:45:24 +0530

    first commit

---
+++ routes/api/users.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
+
+const User = require("../../models/User");
+
+//@route   Post api/users
+//@desc    Register user
+//@access  Public
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include validate email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      //see if user exists
+      let user = await User.findOne({ email: email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      //get users gravatar url
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      //encrypt password using bcrypt
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      //return jsonwebtoken
+      res.send("User registered");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const helmet = require("helmet");
+const connectDB = require("./config/db");
+
+const app = express();
+
+app.use(helmet());
+
+//connect database
+connectDB();
+
+//Init Middleware
+app.use(express.json({ extended: false }));
+//app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => res.send("API RUNNING"));
+
+//Define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
